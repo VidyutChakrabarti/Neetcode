534462019ab2c6e83b88c255a97e4048e816b772
VidyutChakrabarti <chakrabartivr@rknec.edu>
2024-11-19 18:58:34 +0530

    Dynamic programming

---
+++ Blind 150/DP/maxproductsubArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int res = nums[0];
+        int currMax = 1, currMin = 1; 
+        for(int num: nums){
+            int temp = num * currMax;
+            currMax = Math.max(Math.max(currMax*num, currMin*num), num); 
+            currMin = Math.min(Math.min(temp, currMin*num),num);
+            res = Math.max(res, currMax); 
+        }
+        return res;
+    }
+}
