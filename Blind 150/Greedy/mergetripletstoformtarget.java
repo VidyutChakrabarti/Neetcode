6cb8b5e0afcab80feac719f03c5be143a3533df0
VidyutChakrabarti <chakrabartivr@rknec.edu>
2024-11-22 18:58:34 +0530

    Greedy

---
+++ Blind 150/Greedy/mergetripletstoformtarget.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        Set<Integer> good = new HashSet<>();
+
+        for (int[] t : triplets) {
+            if (t[0] > target[0] || t[1] > target[1] || t[2] > target[2]) {
+                continue;
+            }
+            for (int i = 0; i < t.length; i++) {
+                if (t[i] == target[i]) {
+                    good.add(i);
+                }
+            }
+        }
+        return good.size() == 3;
+    }
+}
+
+
+/*optimal greedy
+
+public class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        boolean x = false, y = false, z = false;
+        for (int[] t : triplets) {
+            x |= (t[0] == target[0] && t[1] <= target[1] && t[2] <= target[2]);
+            y |= (t[0] <= target[0] && t[1] == target[1] && t[2] <= target[2]);
+            z |= (t[0] <= target[0] && t[1] <= target[1] && t[2] == target[2]);
+            if (x && y && z) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+*/
