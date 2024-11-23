6ca4701c4019480846a840e755d91cac628d85ea
VidyutChakrabarti <chakrabartivr@rknec.edu>
2024-11-23 14:58:34 +0530

    Intervals

---
+++ Blind 150/Intervals/MergeInterval.java
@@ -0,0 +1,101 @@
+//SWEEP LINE ALGORITHM
+
+public class Solution {
+    public int[][] merge(int[][] intervals) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        
+        for (int[] interval : intervals) {
+            map.put(interval[0], map.getOrDefault(interval[0], 0) + 1);
+            map.put(interval[1], map.getOrDefault(interval[1], 0) - 1);
+        }
+        
+        List<int[]> res = new ArrayList<>();
+        int have = 0;
+        int[] interval = new int[2];
+        
+        for (int point : map.keySet()) {
+            if (have == 0) interval[0] = point;
+            have += map.get(point);
+            if (have == 0) {
+                interval[1] = point;
+                res.add(new int[] {interval[0], interval[1]});
+            }
+        }
+        
+        return res.toArray(new int[res.size()][]);
+    }
+}
+
+/*
+GREEDY: 
+public class Solution {
+    public int[][] merge(int[][] intervals) {
+        int max = 0;
+        for (int i = 0; i < intervals.length; i++) {
+            max = Math.max(intervals[i][0], max);
+        }
+
+        int[] mp = new int[max + 1];
+        for (int i = 0; i < intervals.length; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            mp[start] = Math.max(end + 1, mp[start]);
+        }
+
+        int r = 0;
+        int have = -1;
+        int intervalStart = -1;
+        for (int i = 0; i < mp.length; i++) {
+            if (mp[i] != 0) {
+                if (intervalStart == -1) intervalStart = i;
+                have = Math.max(mp[i] - 1, have);
+            }
+            if (have == i) {
+                intervals[r++] = new int[] { intervalStart, have };
+                have = -1;
+                intervalStart = -1;
+            }
+        }
+
+        if (intervalStart != -1) {
+            intervals[r++] = new int[] { intervalStart, have };
+        }
+        if (intervals.length == r) {
+            return intervals;
+        }
+
+        int[][] res = new int[r][];
+        for (int i = 0; i < r; i++) {
+            res[i] = intervals[i];
+        }
+
+        return res;
+    }
+}
+
+SORTING: 
+
+public class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> output = new ArrayList<>();
+        output.add(intervals[0]);
+
+        for (int[] interval : intervals) {
+            int start = interval[0];
+            int end = interval[1];
+            int lastEnd = output.get(output.size() - 1)[1];
+
+            if (start <= lastEnd) {
+                output.get(output.size() - 1)[1] = Math.max(lastEnd, end);
+            } else {
+                output.add(new int[]{start, end});
+            }
+        }
+        return output.toArray(new int[output.size()][]);
+    }
+}
+
+
+
+*/
