e739f0bfaecc54b8359fd6f0d548d8136c8bfc81
VidyutChakrabarti <chakrabartivr@rknec.edu>
2024-11-24 14:58:34 +0530

    bit manipulation

---
+++ Blind 150/Bit Manipulation/nonCyclicalnumber.java
@@ -0,0 +1,84 @@
+// Hashset Apporach
+
+public class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> visit = new HashSet<>();
+
+        while (!visit.contains(n)) {
+            visit.add(n);
+            n = sumOfSquares(n);
+            if (n == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int sumOfSquares(int n) {
+        int output = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            digit = digit * digit;
+            output += digit;
+            n /= 10;
+        }
+        return output;
+    }
+}
+
+/*
+
+fast and slow pointers -1: 
+
+public class Solution {
+    public boolean isHappy(int n) {
+        int slow = n, fast = sumOfSquares(n);
+
+        while (slow != fast) {
+            fast = sumOfSquares(fast);
+            fast = sumOfSquares(fast);
+            slow = sumOfSquares(slow);
+        }
+
+        return fast == 1;
+    }
+
+    private int sumOfSquares(int n) {
+        int output = 0;
+        while (n != 0) {
+            output += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return output;
+    }
+}
+
+fast and slow pointers - 2:
+
+public class Solution {
+    public boolean isHappy(int n) {
+        int slow = n, fast = sumOfSquares(n);
+        int power = 1, lam = 1;
+        while (slow != fast) {
+            if (power == lam) {
+                slow = fast;
+                power *= 2;
+                lam = 0;
+            }
+            lam++;
+            fast = sumOfSquares(fast);
+        }
+
+        return fast == 1;
+    }
+
+    private int sumOfSquares(int n) {
+        int output = 0;
+        while (n != 0) {
+            output += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return output;
+    }
+}
