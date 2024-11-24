57b92df25cf2e640c9a6759f3f27002f522e5f0d
VidyutChakrabarti <chakrabartivr@rknec.edu>
2024-11-24 14:58:34 +0530

    bit manipulation

---
+++ Blind 150/Bit Manipulation/rotateImage.java
@@ -0,0 +1,85 @@
+//reverse and transpose
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        // Reverse the matrix vertically
+        reverse(matrix);
+
+        // Transpose the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    private void reverse(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[n - 1 - i];
+            matrix[n - 1 - i] = temp;
+        }
+    }
+}
+
+
+/*
+rotate by four cells
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int l = 0;
+        int r = matrix.length - 1;
+        
+        while ( l < r ) {
+            for(int i = 0; i < r - l; i++) {
+                int top = l;
+                int bottom = r;
+                 //save the topleft
+                int topLeft = matrix[top][l + i];
+
+                //move bottom left into top left
+                matrix[top][l + i] = matrix[bottom - i][l];
+
+                // move bottom right into bottom left
+                matrix[bottom - i][l] = matrix[bottom][r - i];
+
+                // move top right into bottom right
+                matrix[bottom][r - i] = matrix[top + i][r];
+
+                // move top left into top right
+                matrix[top + i][r] = topLeft;
+                
+            }
+            r--;
+            l++;
+        }
+    }
+}
+
+brute force
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] rotated = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = rotated[i][j];
+            }
+        }
+    }
+}
+
+
+*/
