4c5888f1af721220bc369906dbcaa13aee7b32c7
VidyutChakrabarti <chakrabartivr@rknec.edu>
2024-11-25 15:58:34 +0530

    math and geo

---
+++ Blind 150/Bit Manipulation/singleNumber.java
@@ -0,0 +1,50 @@
+//Bit manipulation
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+}
+
+/*
+
+HashSet
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                seen.remove(num);
+            } else {
+                seen.add(num);
+            }
+        }
+        return seen.iterator().next();
+    }
+}
+
+
+Sorting
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0;
+        while (i < nums.length - 1) {
+            if (nums[i] == nums[i + 1]) {
+                i += 2;
+            } else {
+                return nums[i];
+            }
+        }
+        
+        return nums[i];
+    }
+}
+
+*/
