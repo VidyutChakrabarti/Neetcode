15f1315dc64453547e254cb405675a0532b41f55
VidyutChakrabarti <chakrabartivr@rknec.edu>
2024-11-25 15:58:34 +0530

    math and geo

---
+++ Blind 150/Bit Manipulation/sumoftwo integers.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int getSum(int a, int b) {
+        int carry = 0, res = 0, mask = 0xFFFFFFFF;
+
+        for (int i = 0; i < 32; i++) {
+            int a_bit = (a >> i) & 1;
+            int b_bit = (b >> i) & 1;
+            int cur_bit = a_bit ^ b_bit ^ carry;
+            carry = (a_bit + b_bit + carry) >= 2 ? 1 : 0;
+            if (cur_bit != 0) {
+                res |= (1 << i);
+            }
+        }
+
+        if (res > 0x7FFFFFFF) {
+            res = ~(res ^ mask);
+        }
+        
+        return res;
+    }
+}
+
+
+/*
+public class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = (a & b) << 1;
+            a ^= b;
+            b = carry;
+        }
+        return a;
+    }
+}
+
+*/
